c8353b06f50c2cdb336632765669cbb98a7b1bae
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2021-11-02 10:35:06 +0100

    feat: Voeg ReSpec in

---
+++ docs/framework/config.js
@@ -0,0 +1,14 @@
+var respecConfig = { // => https://github.com/stichting-crow/respec/wiki
+    specType: "CROW-DOC",
+    specStatus: "",
+    pubDomain: "verkeersborden",
+    shortName: "framework",
+    subtitle: "Uitgangspunten, use-case en architectuurframework",
+    editors: [
+        {
+            name: "Elisabeth Klören",
+            company: "CROW",
+        }
+    ],
+    github: "https://github.com/Stichting-CROW/verkeersborden",
+};
