96fd7a0179f776221e95eeeeb0f74230ba056892
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2022-06-09 17:08:11 +0200

    chore: upgrade respec-template
    Maakt PDF knop beschikbaar #109

--- docs/framework/config.js
+++ docs/framework/config.js
@@ -1,18 +1,18 @@
-var respecConfig = { // => https://github.com/stichting-crow/respec/wiki
-    specType: "CROW-DOC",
-    specStatus: "wd",
-    pubDomain: "verkeersborden",
-    shortName: "framework",
-    subtitle: "Uitgangspunten, use-case en architectuurframework",
-    editors: [
-        {
-            name: "Elisabeth Klören",
-            company: "CROW",
-        },
-        {
-            name: "Redmer Kronemeijer",
-            company: "CROW"
-        }
-    ],
-    github: "https://github.com/Stichting-CROW/verkeersborden",
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  pubDomain: "verkeersborden",
+  shortName: "framework",
+  subtitle: "Uitgangspunten, use-case en architectuurframework",
+  editors: [
+    {
+      name: "Elisabeth Klören",
+      company: "CROW",
+    },
+    {
+      name: "Redmer Kronemeijer",
+      company: "CROW",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/verkeersborden",
 };
