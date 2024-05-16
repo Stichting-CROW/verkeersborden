89e50dfd43f74fa724c64c9fc6428824639743be
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2024-05-16 14:25:35 +0200

    update LD

--- framework/config.js
+++ framework/config.js
@@ -77,7 +77,7 @@ function indexOfRequirements(config) {
 
 var respecConfig = {
   // => https://github.com/stichting-crow/respec/wiki
-  specStatus: "DEF",
+  specStatus: "LD",
   pubDomain: "verkeersborden",
   shortName: "framework",
   license: "CC-BY",
