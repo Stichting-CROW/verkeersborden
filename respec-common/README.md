24ac4496232f1925f9000efb387d3a1263859562
redmer <redmer@users.noreply.github.com>
2025-01-31 10:16:18 +0000

    Deploying to docs.crow.nl from @
    Reason: After push by redmer

---
+++ respec-common/README.md
@@ -0,0 +1,3 @@
+# Common files for ReSpec specifications
+
+This repository is meant to be used as a submodule in multiple repositories that use CROW's ReSpec.
---
+++ respec-common/local-biblio.js
@@ -0,0 +1,153 @@
+var localBibliography = {
+  BOMOS_fundament: {
+    href: "https://gitdocumentatie.logius.nl/publicatie/bomos/fundament/",
+    publisher: "Logius",
+    status: "Actueel",
+    title: "BOMOS, het fundament",
+  },
+  BOMOS_verdieping: {
+    href: "https://gitdocumentatie.logius.nl/publicatie/bomos/verdieping/",
+    publisher: "Logius",
+    status: "Actueel",
+    title: "BOMOS, de verdieping",
+  },
+  CROW_279: {
+    href: "https://www.crow.nl/publicaties/karakteristieken-van-voertuigen-en-mensen",
+    publisher: "CROW",
+    status: "Actueel",
+    title: "Karakteristieken van voertuigen en mensen",
+  },
+  CROW_96a_en_96b: {
+    href: "https://www.crow.nl/online-kennis-tools/kennismodule-wiu",
+    publisher: "CROW",
+    status: "Actueel",
+    title: "Werk in uitvoering",
+  },
+  DIN_67520_2013_10: {
+    href: "https://www.baunormenlexikon.de/norm/din-67520/014ab8f6-7c8e-4f96-be3c-d91ca3d39355",
+    publisher: "Bauwnormen Institut DeutschlandN",
+    status: "Actueel",
+    title:
+      "Retroreflektierende Materialien zur Verkehrssicherung - Lichttechnische Mindestanforderungen an Reflexstoffe",
+  },
+  ISO_IEC_IEEE_15288_2015: {
+    href: "https://www.iso.org/standard/63711.html",
+    publisher: "ISO",
+    status: "Actueel",
+    title:
+      "ISO/IEC/IEEE 15288 Systems and software engineering - System life cycle processes",
+  },
+  ISO_IEC_IEEE_29148_2018: {
+    href: "https://www.iso.org/standard/72089.html",
+    publisher: "ISO",
+    status: "Actueel",
+    title:
+      "ISO/IEC/IEEE 29148:2018 Systems and software engineering — Life cycle processes — Requirements engineering",
+  },
+  Leidraad_SE_3: {
+    href: "https://www.leidraadse.nl/assets/files/downloads/LeidraadSE/V3/Leidraad_V3_SE_web.pdf",
+    publisher: "Leidraad SE",
+    status: "Actueel",
+    title: "Leidraad SE versie 3",
+  },
+  "NEN3381:2020": {
+    href: "https://www.nen.nl/nen-3381-2020-nl-267378",
+    publisher: "NEN",
+    status: "Actueel",
+    title: "Wegmeubilair - Eisen voor permanente en tijdelijke verkeersborden",
+  },
+  NEN3381_2020: {
+    href: "https://www.nen.nl/nen-3381-2020-nl-267378",
+    publisher: "NEN",
+    status: "Actueel",
+    title: "Wegmeubilair - Eisen voor permanente en tijdelijke verkeersborden",
+  },
+  NEN_2660_1_2022: {
+    href: "https://www.nen.nl/en/nen-2660-1-2022-nl-291666",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "NEN 2660-1 Rules for information modelling of the built environment - Part 1: Conceptual models",
+  },
+  NEN_2660_2_2022: {
+    href: "https://www.nen.nl/en/nen-2660-2-2022-nl-291667",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "NEN 2660-2 Regels voor informatiemodellering van de gebouwde omgeving - Deel 2: Praktische configuratie, extensie en implementatie van NEN 2660-1",
+  },
+  NEN_3610: {
+    href: "https://www.nen.nl/nen-3610-2022-nl-296137",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "Basismodel geo-informatie - Termen, definities, relaties en algemene regels voor de uitwisseling van informatie over aan de aarde gerelateerde ruimtelijke objecten",
+  },
+  NEN_EN_12899_1_2007: {
+    href: "https://www.nen.nl/en/nen-en-12899-1-2007-nl-144673",
+    publisher: "NEN",
+    status: "Actueel",
+    title: "Fixed, vertical road traffic signs - Part 1: Fixed signs",
+  },
+  NEN_EN_ISO_14823_1: {
+    href: "https://www.nen.nl/en/nen-en-iso-14823-1-2024-en-324952",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "Intelligent transport systems - Graphic data dictionary - Part 1: Specification",
+  },
+  NEN_EN_ISO_19650_1_2019: {
+    href: "https://www.nen.nl/nen-en-iso-19650-1-2019-nl-275305",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "Organisatie en digitalisering van informatie over gebouwen en civieltechnische werken, met inbegrip van building information modelling (BIM) - Informatiemanagement met behulp van bouw informatie modelling - Deel 1: Concepten en beginselen",
+  },
+  NEN_EN_ISO_19650_2_2019: {
+    href: "https://www.nen.nl/nen-en-iso-19650-2-2019-nl-275518",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "Organisatie en digitalisering van informatie over gebouwen en civieltechnische werken, met inbegrip van building information modelling (BIM) - Informatiemanagement met behulp van building information modelling - Deel 2: Leveringsfase van de assets",
+  },
+  NEN_EN_ISO_19650_3_2020: {
+    href: "https://www.nen.nl/nen-en-iso-19650-3-2020-en-275488",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "Organization and digitization of information about buildings and civil engineering works, including building information modelling (BIM) - Information management using building information modelling - Part 3: Operational phase of the assets",
+  },
+  NEN_EN_ISO_19650_4_2022: {
+    href: "https://www.nen.nl/nen-en-iso-19650-4-2022-en-301119",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "Organization and digitization of information about buildings and civil engineering works, including building information modelling (BIM) - Information management using building information modelling - Part 4: Information exchange",
+  },
+  NEN_EN_ISO_55000_2014: {
+    href: "https://www.nen.nl/nen-iso-55000-2014-nl-193078",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "ISO 55000:2014 Asset management — Overview, principles and terminology",
+  },
+  NEN_EN_ISO_9000_2015: {
+    href: "https://www.nen.nl/en/nen-en-iso-9000-2015-en-nl-211925",
+    publisher: "NEN",
+    status: "Actueel",
+    title:
+      "NEN-EN-ISO 9000:2015 Kwaliteitsmanagementsystemen - Grondbeginselen en verklarende woordenlijst",
+  },
+  kenkrechmer: {
+    href: "https://www.csrstds.com/OpnStdsCallforAction.pdf",
+    publisher: "University of Colorado",
+    status: "Actueel",
+    title: "Open standaards call for action",
+  },
+  whitepaper_ontology_alignment: {
+    href: "https://docs.crow.nl/ontology-alignment/whitepaper/",
+    publisher: "CROW",
+    status: "Actueel",
+    title: "Ontology Matching trough alignment and extension: a Best Practice",
+  },
+};
