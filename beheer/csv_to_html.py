15636d7939967d4f78e12ab775f261f107f8d056
gulaycanbaloglu <gulaycanbaloglu@semmtech.nl>
2025-08-11 15:48:21 +0200

    update script

--- beheer/csv_to_html.py
+++ beheer/csv_to_html.py
@@ -1,29 +1,43 @@
 import pandas as pd 
 
+CONCEPT_NAME = "iconName" # 'fysiekePlaatNameNL'
+CONCEPT_DESCR = "iconDefinition" # "fysiekePlaatDefinition"
+
 # Put the script and your input file in the same folder, or provide the full path below.
-csv_file_name = "tableDownload.csv"
+csv_file_name = "beheer/tableDownloadIcon.csv"
 text_separator = '"'
-output_file = "output.html"
+output_file = "beheer/outputIcons.html"
+
 
 df = pd.read_csv(csv_file_name, quotechar=text_separator)
-df = df.sort_values(['fysiekePlaatNameNL'])
+df = df.sort_values([CONCEPT_NAME])
 
 with open(output_file, "w", encoding="utf-8") as file:
     file.write("<table>\n")  # Start table
+
+    col_count = 0
     file.write("  <tr>\n")  # Start row
     
     for index, row in df.iterrows():
         
-        name = str(row["fysiekePlaatNameNL"]) if not pd.isnull(row["fysiekePlaatNameNL"]) else ""
-        description = str(row["fysiekePlaatDefinition"]) if not pd.isnull(row["fysiekePlaatDefinition"]) else ""
+        name = str(row[CONCEPT_NAME]) if not pd.isnull(row[CONCEPT_NAME]) else ""
+        description = str(row[CONCEPT_DESCR]) if not pd.isnull(row[CONCEPT_DESCR]) else ""
         svg = str(row["svg"]) if not pd.isnull(row["svg"]) else ""
 
         file.write(f"    <td><figure>\n")
         file.write(f"      <img src=\"{svg}\" alt=\"{description}\" >\n")
         file.write(f"      <figcaption>{name}</figcaption>\n")
         file.write(f"    </figure></td>\n")
-    
-    file.write("  </tr>\n")  # End row
+
+        col_count += 1
+        if col_count == 4:  # After 4 columns, close row and start a new one
+            file.write("  </tr>\n")
+            file.write("  <tr>\n")
+            col_count = 0
+
+    if col_count != 0:
+        file.write("  </tr>\n")
+    # file.write("  </tr>\n")  # End row
     file.write("</table>\n")  # End table
 
 print(f"HTML file '{output_file}' generated successfully!")
